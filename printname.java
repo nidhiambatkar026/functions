4ef970507babe74d65cff30b3cd58d7d685f7c8b
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-01-20 23:33:04 +0530

    name printed using function

---
+++ printname.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class printname {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+
+        printMyName(name);
+    }
+}
